4de240bd55aee8631e8e671821a01af9ecacb411
JBPlinn <bardon.pro@gmail.com>
2019-09-10 15:03:33 +0200

    fix list_size for each list

--- linkedlist.c
+++ linkedlist.c
@@ -11,13 +11,17 @@ struct node
 
 struct list 
 {
-  Node * head; 
+  Node *       head;
+  unsigned int list_size;
 };
 
-unsigned int list_size;
 
-Node * createnode(uint16_t data);
-
+/**
+ * Single node creation with (slow) memory allocation.
+ *
+ * @param data the data to store in the list
+ * @return     pointer to the node or NULL upon error
+ */
 Node * createnode(uint16_t data)
 {
   Node * newNode = (Node *) malloc(sizeof(Node));
@@ -28,11 +32,15 @@ Node * createnode(uint16_t data)
   }
   newNode->data = data;
   newNode->next = NULL;
-  list_size++;
 
   return newNode;
 }
 
+/**
+ * Create the list.
+ *
+ * @return  pointer to the list or NULL upon error
+ */
 List * ListMake()
 {
   List * list = (List *) malloc(sizeof(List));
@@ -44,33 +52,19 @@ List * ListMake()
   
   list->head = NULL;
   
-  list_size = 0;
+  list->list_size = 0;
   
   return list;
 }
 
-void ListDisplay(List * list) 
-{
-  Node * current = list->head;
-  
-  if(list->head == NULL) 
-  {
-    return;
-  }
-  
-  printf("Size = %d\n", list_size);
-  
-  for(; current != NULL; current = current->next) 
-  {
-    printf("%d-", current->data);
-  }
-  printf("\n");
-}
 
 /**
- *  add unique data to list
+ * Add unique data to the list.
  * 
- * */
+ * @param   data data to insert
+ * @param   list pointer to the list
+ * @return  true if insertion succeeded, false if the value already in the list
+ */
 bool ListAdd(uint16_t data, List * list)
 {
   bool ok = false;
@@ -79,6 +73,7 @@ bool ListAdd(uint16_t data, List * list)
   if(list->head == NULL)
   {
     list->head = createnode(data);
+    list->list_size++;
     ok = true;
   }
   else 
@@ -95,12 +90,20 @@ bool ListAdd(uint16_t data, List * list)
       current = current->next;
     }
     current->next = createnode(data);
+    list->list_size++;
     ok = true;
   }
 
   return ok;
 }
 
+/**
+ * Delete a dat a from the list
+ *
+ * @param   data the data to delete
+ * @param   list pointer to the list
+ * @return  true if the data has been deleted, false if data is not present in the list
+ */
 bool ListDelete(uint16_t data, List * list)
 {
   bool ok = false;
@@ -118,7 +121,9 @@ bool ListDelete(uint16_t data, List * list)
       }
       
       free(current);
-      list_size--;
+      list->list_size--;
+      if(list->list_size < 0)
+         list->list_size = 0;
       ok = true;
       
       return ok;
@@ -130,6 +135,12 @@ bool ListDelete(uint16_t data, List * list)
   return ok;                               
 }                                   
 
+/**
+ * Reverse the order of the list.
+ * 1st item will be the last.
+ *
+ * @param list pointer to the list.
+ */
 void ListReverse(List * list)
 {
   Node * reversed = NULL;
@@ -147,6 +158,11 @@ void ListReverse(List * list)
   list->head = reversed;
 }
 
+/**
+ * Remove all elements from the list an free list memory.
+ *
+ * @param list pointer to the list.
+ */
 void ListDestroy(List * list)
 {
   Node * current = list->head;
@@ -160,4 +176,40 @@ void ListDestroy(List * list)
   }
   
   free(list);
+}
+
+/**
+ * get the actual size of the list
+ *
+ * @param list pointer to the list.
+ * @return     size of the list
+ */
+unsigned int ListSize(List * list)
+{
+  return list->list_size;
+}
+
+
+/**
+ * Display the size of the list and all elements in order
+ * to the console.
+ *
+ * @param list pointer to the list
+ */
+void ListDisplay(List * list)
+{
+  Node * current = list->head;
+
+  printf("Size = %d\n", list->list_size);
+
+  if(list->head == NULL)
+  {
+    return;
+  }
+
+  for(; current != NULL; current = current->next)
+  {
+    printf("%d-", current->data);
+  }
+  printf("\n");
 }
--- linkedlist.h
+++ linkedlist.h
@@ -26,13 +26,17 @@ bool ListAdd(uint16_t data, List * list);
 /// delete an element from the list
 bool ListDelete(uint16_t data, List * list);
 
-/// display the list values
-void ListDisplay(List * list);
-
 /// reverse the list order
 void ListReverse(List * list);
 
 /// destroy the list
 void ListDestroy(List * list);
+
+/// get the size of the list
+unsigned int ListSize(List * list);
+
+/// display the list values
+void ListDisplay(List * list);
+
 
 #endif
